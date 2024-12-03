1d37c88b9908ffcf61223a211c12e05915a59fce
TRISTANO13 <tm1313@yahoo.fr>
2024-12-03 23:24:11 +0100

    Update image API to serve images from a static 'chaewon' folder and adjust fetch URL in HTML

--- backend.py
+++ backend.py
@@ -5,23 +5,23 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/images/<folder_name>', methods=['GET'])
-def get_images(folder_name):
-    IMAGE_FOLDER = os.path.join(os.getcwd(), folder_name)
-    
+# Dossier où se trouvent les images
+IMAGE_FOLDER = 'chaewon'
+
+@app.route('/api/chaewon', methods=['GET'])
+def get_images():
+    IMAGE_FOLDER = 'chaewon'
     # Récupérer la liste des fichiers d'images dans le dossier
     images = []
-    if os.path.exists(IMAGE_FOLDER):
-        for filename in os.listdir(IMAGE_FOLDER):
-            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-                images.append(request.host_url + 'images/' + filename)
-    
+    for filename in os.listdir(IMAGE_FOLDER):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            images.append(request.host_url + 'images/' + filename)
     return jsonify(images)
+
 
 @app.route('/images/<path:filename>', methods=['GET'])
 def serve_image(filename):
     # Servir une image directement depuis le dossier des images
-    IMAGE_FOLDER = os.path.dirname(os.path.abspath(filename))
     return send_from_directory(IMAGE_FOLDER, filename)
 
 if __name__ == '__main__':
